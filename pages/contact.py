f987295534f4214c558666d7d69de223f58d7083
romi-sai <54590905+romi-sai@users.noreply.github.com>
2023-12-23 01:53:09 -0500

    Create contact.py

---
+++ pages/contact.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Contact Me!")
+st.subheader('Romika Sairam')
+st.write('University at Buffalo')
+st.write('School of Engineering and Applied Sciences')
+st.write('email: romikasa@buffalo.edu')
